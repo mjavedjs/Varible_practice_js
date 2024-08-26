a99d5a2766e48953cd145b6340934182dc3fb5fd
muhammadjaved238 <mjavedjs2006@gmail.com>
2024-08-26 12:07:07 +0500

    variable_practice

---
+++ app.js
@@ -0,0 +1,37 @@
+// let name="pizza   pizz    piz   pi    p" ;
+// alert(name);
+// question 2;
+// let email="mjavedjs2006@gmail.com";
+// alert("my email is "  +  email)
+// Qustion no 3
+// let book="A smarter way to learn JavaScript";
+// alert(book)
+// Question no 4
+// let strig=' Write a script to display this in browser through JS'
+// document.write(strig)
+// Question no 5
+
+// let desing="---------╰(*°▽°*)╯(☞ﾟヮﾟ)☞╰(*°▽°*)╯---------"
+// alert(desing)
+// Question no 6
+// let age=18;
+// alert("my age is "   +age)
+// Question no 7
+// let visitor= +prompt("enter visitor");
+// alert("You have viste theis web sit:"      +  visitor);
+// Question no 8
+// let birth_year= +prompt("enter you birth year");
+// document.write("my Birth  Year is " +   birth_year);
+// Question no  9
+let visitor_name=prompt("enter the visitor Name");
+let Product_title=prompt("Enter Proudct T-shirt");
+let Quantity=+prompt("enter Quantity of Prouduct");
+let outlit=prompt("enter outlint Name");
+document.write("The customer Name is " + visitor_name + 
+    ". The Product is " + Product_title + ". The Quantity of Product is " + Quantity + ". From " + outlit);
+
+
+
+
+
+
